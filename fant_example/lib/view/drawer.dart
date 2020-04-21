306b8925418ad459393d3f6cd6604cab1424726a
尹旭 <yin_8023@addcn.com>
2020-04-21 15:09:18 +0800

    截图

--- fant_example/lib/view/drawer.dart
+++ fant_example/lib/view/drawer.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:example/comm/comm.dart';
 import 'package:fant/button/index.dart';
 import 'package:fant/fant.dart';
@@ -9,9 +11,61 @@ class DrawerDemo extends StatefulWidget {
 }
 
 class DrawerDemoState extends State<DrawerDemo> {
-  @override
-  void initState() {
-    super.initState();
+  Widget drawerView({VoidCallback tap}) {
+    List<Widget> children = [];
+    children.add(Container(
+      height: 100,
+      alignment: Alignment.center,
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      child: Row(
+        children: <Widget>[
+          FRound(child: Icon(Icons.supervised_user_circle, size: 40), type: FRoundType.Round, size: 40),
+          Expanded(
+            child: Container(
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.symmetric(horizontal: 8),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Container(
+                    child: Text('title'),
+                    alignment: Alignment.centerLeft,
+                  ),
+                  Container(
+                    child: Text('subTitle'),
+                    alignment: Alignment.centerLeft,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    ));
+    children.add(Expanded(
+      child: Container(
+        color: Theme.of(context).backgroundColor,
+        padding: EdgeInsets.only(top: 16),
+        child: Column(
+          children: <Widget>[
+            ListTile(title: Text('菜单1')),
+            ListTile(title: Text('菜单2')),
+            ListTile(title: Text('菜单3')),
+            ListTile(title: Text('菜单4')),
+            ListTile(title: Text('菜单5')),
+            ListTile(title: Text('菜单6')),
+          ],
+        ),
+      ),
+    ));
+
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.8,
+      alignment: Alignment.center,
+      color: Colors.green,
+      padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
+      child: Column(children: children),
+    );
   }
 
   @override
@@ -64,15 +118,7 @@ class DrawerDemoState extends State<DrawerDemo> {
                 tap = FDrawer.showDrawer(
                   context,
                   type: FDrawerType.left,
-                  child: Container(
-                    width: MediaQuery.of(context).size.width * 0.8,
-                    color: Colors.red,
-                    alignment: Alignment.center,
-                    child: GestureDetector(
-                      onTap: close,
-                      child: Text('hello world'),
-                    ),
-                  ),
+                  child: drawerView(tap: close),
                 );
               },
             ),
@@ -89,15 +135,7 @@ class DrawerDemoState extends State<DrawerDemo> {
                 tap = FDrawer.showDrawer(
                   context,
                   type: FDrawerType.right,
-                  child: Container(
-                    width: MediaQuery.of(context).size.width * 0.8,
-                    color: Colors.red,
-                    alignment: Alignment.center,
-                    child: GestureDetector(
-                      onTap: close,
-                      child: Text('hello world'),
-                    ),
-                  ),
+                  child: drawerView(tap: close),
                 );
               },
             ),
